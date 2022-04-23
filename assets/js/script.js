5e0229d1926f70935e13ff44264e9c1d2db6ae8d
volausson <veronicagolausson@gmail.com>
2022-04-23 17:04:46 +0000

    add eventlistener an on-click property to button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,30 @@
+//Get the button element and add event listeners 
 
-/**Generate random number from 1 to 6 */
+document.addEventListener('DOMContentLoaded', function() {
+    let button = document.getElementsByTagName 'button');
+
+    for (let button of buttons) {
+        button.addEventListener ('click', function() {
+            if(this.getAttribute
+               ('data-type') === 'pushToPlay' );
+            
+            });
+        }
+
+//Generate random number from 1 to 6 
+function 
+
+let firstRandomNum = Math.floor(Math.RandomNum()* 6) + 1
+let secondRandomNum = Math.floor(Math.RandomNum()* 6) + 1
+
+//generate img dice1 to dice6
+
+let firstDiceImage = 'assets/dice' + firstRandomNum +'.png';
+document.querySelectorAll('img')[0].setAttribute('src', firstDiceImage);
+
+
+
+
+
+
+
