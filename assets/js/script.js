20a5ac7c7ad62f0d597202d035f26dac69176650
volausson <veronicagolausson@gmail.com>
2022-04-26 10:51:18 +0000

    add eventlistener to play button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,28 @@
 //Get the button element and add event listeners 
 
 document.addEventListener('DOMContentLoaded', function() {
-    let button = document.getElementsByTagName 'button');
+    let button = document.getElementsById'button');
+    for (let button of button) {
+        button.addEventListener('click',function() {
+            let gameType = this.getAttribute('data-type');
+            runGameType(gameType);
+        });
+    }
+    throwDice('play');
 
-    
 
-
-    function throwDice() {
+    function throwDice(play); {
 
         //generate random number//
         const firstRandomNum = Math.floor(Math.RandomNum()* 6) + 1
         const secondRandomNum = Math.floor(Math.RandomNum()* 6) + 1
+
+        if (gametype==='play') {
+            displayAction(firstRandomNum,secondRandomNum)
+        } else {
+            alert(`Unknown game type:${gameType}`;)
+        }
+
 
         //set image src//
     document.getElementById("gamedice").src = "assets/images/dice" + firstRandomNum + ".png";
